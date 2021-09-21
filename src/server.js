e3efca47638ff19f8fca9a2c3e54240fd0c38fda
순일 홍 <sun-ilhong@hhong-2.local>
2021-09-21 20:41:11 +0900

    WebSocket 1

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
-import express from "express";
+import http from "http";
+import WebSocket from "ws";
+import express from "express"; 
 
 const app = express();
 
@@ -17,5 +19,28 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
+// app.listen(3000, handleListen);
 
-app.listen(3000, handleListen);
+// for http + ws 
+const server = http.createServer(app);
+
+// it sends var server to wss server, 
+// same server(port) with 2 protocol(http, ws)
+// connection
+const wss = new WebSocket.Server({ server });
+
+// event listen for wss server
+// socket = browser
+wss.on("connection", (socket) => {
+    console.log("Connect to Browser ✅");
+    // socket event, not server
+    // each browser who connected server
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+    socket.on("message", (message) => {
+        const translatedMessageData = message.toString('utf8');
+        console.log(translatedMessageData);
+    });
+    socket.send("hello!");
+})
+
+server.listen(3000, handleListen);
