09847f99d72913d4946f51809b4b775d606959d2
hhong0326 <hhong0326@naver.com>
2021-09-27 11:21:08 +0900

    Test WebRTC p2p icecandidate process using localtunnel and STUN server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -88,6 +88,14 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
     await getMedia(camerasSelect.value);
+    if (myPeerConnection) {
+        // new Track from getMedia
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection.getSenders().find((sender) => sender.track.kind === "video");
+        
+        // sender is controller deals with the sent data of the other browser
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
@@ -125,23 +133,30 @@ socket.on("welcome", async () => {
     console.log("Someone joined");
     const offer = await myPeerConnection.createOffer();
     myPeerConnection.setLocalDescription(offer);
-    console.log("send the offer");
     socket.emit("offer", offer, roomName);
+    console.log("sent the offer");
 });
 
 // guest
-socket.on("offer", (offer) => {
+socket.on("offer", async (offer) => {
     // answer
-    console.log(offer);
+    console.log("received the offer");
     myPeerConnection.setRemoteDescription(offer);
     const answer = await myPeerConnection.createAnswer();
     myPeerConnection.setLocalDescription(answer);
     socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
 });
 
 // host
 socket.on("answer", (answer) => {
+    console.log("received the answer");
     myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+    console.log("received candidate");
+    myPeerConnection.addIceCandidate(ice);
 });
 
 // RTC Code
@@ -149,6 +164,35 @@ socket.on("answer", (answer) => {
 // addStream()
 function makeConnection() {
     // be able to access anyone
-    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: [ // STUN server test
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302",
+                    "stun:stun3.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302",
+                ],
+            },
+        ],
+    });
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
     myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+    // each other
+    socket.emit("ice", data.candidate, roomName);
+    console.log("sent candidate");
+}
+
+function handleAddStream(data) {
+    console.log("got an event from my peer");
+    console.log(data.stream);
+    console.log("my\n", myStream);
+
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
 }
